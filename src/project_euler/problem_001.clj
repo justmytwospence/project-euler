fbd4b2c22fad64ea81f83e91794dd8ac6ab8382c
Spencer Boucher <spencer@spencerboucher.com>
2015-07-14 13:06:39 -0700

    Formatting

--- src/project_euler/problem_001.clj
+++ src/project_euler/problem_001.clj
@@ -3,8 +3,8 @@
 (defn sum-multiples [up-to & divs]
   (letfn [(multiple-of [num] (some zero? (map (partial rem num) divs)))]
     (->> (range up-to)
-      (filter multiple-of)
-      (reduce +))))
+         (filter multiple-of)
+         (reduce +))))
 
 (defn solution-001 []
   (sum-multiples 1000 3 5))
